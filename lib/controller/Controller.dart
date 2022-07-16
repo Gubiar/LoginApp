3c2855f375790915995c4a38ac0c2a69e0dc3436
Gubiar <gubiardev@gmail.com>
2022-07-16 12:22:40 -0300

    first commit

---
+++ lib/controller/Controller.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart' as http;
+import 'package:http/http.dart';
+
+class Controller {
+
+
+  Future<String> postRegister(String mapToSend) async{
+
+    return 'true';
+
+    // Response response = await http.
+    //
+    // try{
+    //   return 'true';
+    // } catch(e){
+    //   debugPrint(e.toString());
+    //   return 'false';
+    // }
+
+  }
+
+}
---
+++ lib/screens/PageLogin.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class PageLogin extends StatelessWidget {
+  const PageLogin({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
